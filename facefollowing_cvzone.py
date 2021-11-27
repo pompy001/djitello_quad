1b64f88e060078fe6bfc725d8a230223c16c5b25
ankit <janbandhu.ankit786@gmail.com>
2021-11-27 12:29:08 +0530

    added centroid Tracking & person Counter using drone codes

--- facefollowing_cvzone.py
+++ facefollowing_cvzone.py
@@ -4,17 +4,15 @@ from cvzone.FaceDetectionModule import FaceDetector
 import cvzone
 
 
-# cap = cv2.VideoCapture(1)
+# cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
 detector = FaceDetector(minDetectionCon=0.7)
 # _,img = cap.read()
 hi,wi =480,640
+# hi , wi = img.shape[0],img.shape[1]
 
 xPID = cvzone.PID([.22,0,0.1],wi//2)
 yPID = cvzone.PID([.27,0,0.1],hi//2,axis=1)
 zPID = cvzone.PID([.003,0,0.003],12000,limit=[-20,13])
-
-
-
 myplotX = cvzone.LivePlot(yLimit=[-100,100],char='X')
 myplotY = cvzone.LivePlot(yLimit=[-100,100],char='Y')
 myplotZ = cvzone.LivePlot(yLimit=[-100,100],char='Z')
@@ -34,11 +32,12 @@ while True:
     img = me.get_frame_read().frame
     img = cv2.resize(img,(640,480))
     img , bboxs = detector.findFaces(img , draw=True)
+   
     if bboxs:
         cx,cy = bboxs[0]['center']
         x , y, w, h = bboxs[0]['bbox']
         area = w*h
-        print(area)
+        # print(area)
 
         xVal =int(xPID.update(cx))
         yVal =int(yPID.update(cy))
@@ -57,16 +56,16 @@ while True:
 
         # cv2.circle(img , (cx,cy),5,(255,0,255),cv2.FILLED)
         # error = wi//2-cx
-        cv2.putText(img,str(bat),(50,100),cv2.FONT_HERSHEY_PLAIN,3,(255,0,255),3)
+        cv2.putText(img,(str(bat)),(50,100),cv2.FONT_HERSHEY_PLAIN,1,(255,0,255),3)
         # cv2.line(img , (wi//2,0),(wi//2,hi),(255,0,0),2)
         # cv2.line(img,(wi//2,hi//2),(cx,cy),(255,0,255),3)
-    imgStacked = cvzone.stackImages([img,imgPlotX,imgPlotY,imgPlotZ],2,.7)
+    imgStacked =cvzone.stackImages([img,imgPlotY,imgPlotZ],2,0.7)
     me.send_rc_control(0,-zVal,-yVal,xVal)
 
     cv2.imshow('imgStacked',imgStacked)
     # cv2.imshow('OUTPUT',img)
     # cv2.imshow('imgPlotx',imgPlotX)
     if cv2.waitKey(5) & 0xff==('q'):
-        me.land()
+        # me.land()
         break
 cv2.destroyAllWindows
--- mapping.py
+++ mapping.py
@@ -82,6 +82,81 @@ def drawPoints(img , points):
     cv2.putText(img, f'({(points[-1][0] - 500)/100},{(points[-1][1]-500)/100})m',(points[-1][0]+10,points[-1][1]+30),cv2.FONT_HERSHEY_PLAIN,1,(255,0,255),1)
 
 
+## PARAMETERS
+fSpeed = 117/18 #forward speed cm/s actual(15 cm/s)
+aSpeed = 360/10 # angilar speed Degree/s
+Interval = 0.25
+dInterval = fSpeed*Interval
+aInterval = aSpeed*Interval
+###################
+x,y=500,500
+a=0
+yaw = 0
+points= []
+
+kp.init()
+me = tello.Tello()
+
+me.connect()
+print(me.get_battery())
+
+
+
+def getKeyBoardInput():
+    lr, fb, ud, yv = 0, 0, 0, 0
+    speed = 15
+    aspeed = 50
+    d = 0
+    global yaw, x, y, a
+
+    if kp.getKey("LEFT") :
+         lr = -speed
+         d = dInterval
+         a = -180
+    elif kp.getKey("RIGHT") :
+         lr = speed
+         d = -dInterval
+         a = 180
+
+
+    if kp.getKey("UP") :
+         fb = speed
+         d = dInterval
+         a = 270
+
+    elif kp.getKey("DOWN") : 
+        fb = -speed
+        d = -dInterval
+        a = -90
+
+    if kp.getKey("w") : ud = speed
+    elif kp.getKey("s") : up = -speed
+
+    if kp.getKey("a"):
+         yv = aspeed
+         yaw -= aInterval
+    if kp.getKey("d"):
+         yv = -aspeed
+         yaw+=aInterval 
+
+    if kp.getKey("q"):  me.land()
+    if kp.getKey("t"): me.takeoff()
+    sleep(Interval)
+
+    a+=yaw
+    x+=int(d*math.cos(math.radians(a)))
+    y+=int(d*math.sin(math.radians(a)))
+
+
+
+
+    return [lr, fb, ud, yv ,x , y]
+
+def drawPoints(img , points):
+    for point in points:
+        cv2.circle(img,(point[0],point[1]),5,(0,0,255),cv2.FILLED)
+    cv2.circle(img,(points[-1]),5,(255,0,255),cv2.FILLED)
+    cv2.putText(img, f'({(points[-1][0] - 500)/100},{(points[-1][1]-500)/100})m',(points[-1][0]+10,points[-1][1]+30),cv2.FONT_HERSHEY_PLAIN,1,(255,0,255),1)
 
 
 while True:
